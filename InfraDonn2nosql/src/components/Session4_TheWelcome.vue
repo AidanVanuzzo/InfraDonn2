7e55557ffaa7a9cb314ec2264fdfdac32d85f504
Aidan Vanuzzo <aidan.vanuzzo@gmail.com>
2025-10-16 09:19:40 +0200

    ajout de npm i events, modification de TheWelcome.vue pour initialiser la base de données

--- InfraDonn2nosql/src/components/Session4_TheWelcome.vue
+++ InfraDonn2nosql/src/components/Session4_TheWelcome.vue
@@ -1,11 +1,5 @@
 <script setup lang="ts">
-import WelcomeItem from './WelcomeItem.vue'
-import DocumentationIcon from './icons/IconDocumentation.vue'
-import ToolingIcon from './icons/IconTooling.vue'
-import EcosystemIcon from './icons/IconEcosystem.vue'
-import CommunityIcon from './icons/IconCommunity.vue'
-import SupportIcon from './icons/IconSupport.vue'
-import { onMounted, ref} from 'vue';
+import { onMounted, ref } from 'vue';
 import PouchDB from 'pouchdb'
 
 declare interface Post {
@@ -14,12 +8,6 @@ declare interface Post {
   attributes: {
     creation_date: any;
   };
-}
-
-const counter = ref(0);
-
-const increment = () => {
-  counter.value++;
 }
 
 // Référence à la base de données
@@ -53,14 +41,7 @@ onMounted(() => {
   fetchData()
 });
 
-const openReadmeInEditor = () => fetch('/__open-in-editor?file=README.md')
 </script>
-
-<template>
-  <h1>Welcome</h1>
-  <p>Counter: {{ counter }}</p>
-  <button @click="increment">+1</button>
-</template>
 
 <template>
   <h1>Fetch Data</h1>
