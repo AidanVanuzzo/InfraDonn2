e9ee4651918dc064b83771bcafeb0e9496630ff9
AidanVanuzzo <aidan.vanuzzo@heig-vd.ch>
2025-12-15 15:04:39 +0100

    system de like fonctionnel

--- InfraDonn2nosql/src/components/TheWelcome.vue
+++ InfraDonn2nosql/src/components/TheWelcome.vue
@@ -6,7 +6,7 @@ import EcosystemIcon from './icons/IconEcosystem.vue'
 import CommunityIcon from './icons/IconCommunity.vue'
 import SupportIcon from './icons/IconSupport.vue'
 import { onMounted, ref } from 'vue';
-import PouchDB from 'pouchdb'
+import PouchDB from 'pouchdb';
 import findPlugin from "pouchdb-find";
 PouchDB.plugin(findPlugin);
 
@@ -14,6 +14,7 @@ declare interface Post {
   _id?: string,
   post_name: string;
   post_content: string;
+  post_like: number;
   attributes: {
     creation_date: any;
   };
@@ -47,9 +48,9 @@ const dataCreateIndex = () => {
     index: {
       fields: ['Comment.post_id']
     }
-  }).then((response) => {
+  }).then((response: any) => {
     console.log(response);
-  }).catch((err) => {
+  }).catch((err: any) => {
     console.log(err);
   })
 }
@@ -77,17 +78,17 @@ const fetchData = () => {
   storage.value.allDocs({
     include_docs: true,
     attachments: true
-  }).then((result) => {
+  }).then((result: { rows: any[] }) => {
     // handle result
     // ici on recoit tout
 
     // mettre uniquement post_content
-    postsData.value = result.rows.map((row) => row.doc).filter((doc) => !!doc.post_content);
+    postsData.value = result.rows.map((row: { doc: any }) => row.doc).filter((doc: { post_content: any }) => !!doc.post_content);
 
     // mettre uniquement comment_content
-    commentsData.value = result.rows.map((row) => row.doc).filter((doc) => !!doc.comment_content);;
-    console.log(result)
-  }).catch(function (err) {
+    commentsData.value = result.rows.map((row: { doc: any }) => row.doc).filter((doc: { comment_content: any }) => !!doc.comment_content);;
+    //console.log(result)
+  }).catch(function (err: any) {
     console.log(err);
   });
 }
@@ -96,28 +97,63 @@ const addDocument = () => {
   console.log(post_name)
   storage.value.post({
     post_name: post_name,
-    post_content: post_content
-  }).then((response) => {
+    post_content: post_content,
+    post_like: 0
+  }).then((response: any) => {
     // handle response
     console.log(response);
     //location.reload();
     fetchData();
     post_content = ""
     post_name = ""
-  }).catch((err) => {
+  }).catch((err: any) => {
     console.log(err);
   });
 }
 
-const addComment = (post) => {
+const addComment = (post: any) => {
   storage.value.post({
     comment_content: comment_content,
     post_id: post._id
-  }).then((response) => {
+  }).then((response: any) => {
     console.log(post._id);
     fetchData();
     comment_content = ""
-  }).catch((err) => {
+  }).catch((err: any) => {
+    console.log(err);
+  })
+}
+
+const addLike = (post: any) => {
+
+  console.log(post.post_name)
+
+  let post_like = post.post_like;
+
+  post_like++
+
+  const likedPost = {
+    ...post,
+    post_like: post_like
+  }
+
+  console.log(likedPost)
+
+
+
+  storage.value.put(likedPost).then((response: any) => {
+    console.log(response);
+    fetchData();
+  }).catch((err: any) => {
+    console.log(err);
+  })
+
+  return;
+
+  storage.value.put(post.post_like++).then((response: any) => {
+    //console.log(post);
+    fetchData();
+  }).catch((err: any) => {
     console.log(err);
   })
 }
@@ -125,9 +161,9 @@ const addComment = (post) => {
 let post_name_change = ""
 let post_content_change = ""
 
-const updateDocument = (post) => {
+const updateDocument = (post: any) => {
 
-  console.log(post_name)
+  console.log(post.post_name)
 
   const updatedPost = {
     ...post,
@@ -136,51 +172,51 @@ const updateDocument = (post) => {
   }
   console.log(updatedPost)
 
-  storage.value.put(updatedPost).then((response) => {
+  storage.value.put(updatedPost).then((response: any) => {
     console.log(response);
     fetchData();
     post_content_change = ""
     post_name_change = ""
     //location.reload();
-  }).catch((err) => {
+  }).catch((err: any) => {
     console.log(err);
   });
 
   return;
-  storage.value.get('mydoc').then(function (doc) {
+  storage.value.get('mydoc').then(function (doc: { _rev: any }) {
     return storage.value.put({
       _id: 'mydoc',
       _rev: doc._rev,
       title: "Let's Dance"
     });
-  }).then(function (response) {
+  }).then(function (response: any) {
     // handle response
     console.log(response);
     fetchData();
-  }).catch(function (err) {
+  }).catch(function (err: any) {
     console.log(err);
   });
 }
 
-const deleteDocument = (post) => {
+const deleteDocument = (post: any) => {
 
   console.log(post._id)
 
-  storage.value.remove(post._id, post._rev).then((result) => {
+  storage.value.remove(post._id, post._rev).then((result: any) => {
     console.log(result);
     fetchData();
     //location.reload();
-  }).catch((err) => {
+  }).catch((err: any) => {
     console.log(err);
   });
   return;
-  storage.value.get('post').then(function (post) {
+  storage.value.get('post').then(function (post: { _id: any; _rev: any }) {
     return storage.value.remove(post._id, post._rev);
-  }).then((result) => {
+  }).then((result: any) => {
     // handle result
     console.log(result);
     fetchData();
-  }).catch(function (err) {
+  }).catch(function (err: any) {
     console.log(err);
   });
 }
@@ -194,7 +230,7 @@ onMounted(() => {
   //deleteDocument()
 });
 
-const getComment = (postId) => {
+const getComment = (postId: any) => {
 
   // que les commentaires de commentDatas qui ont le post ID
   return commentsData.value.filter((doc) => doc.post_id === postId);
@@ -224,8 +260,8 @@ const openReadmeInEditor = () => fetch('/__open-in-editor?file=README.md')
       <button @click="deleteDocument(post)">Delete Post</button>
 
       <p></p>
-      <button @click="increment">Like</button>
-      <p>{{ counter }}</p>
+      <button @click="addLike(post)">Like</button>
+      <p>{{ post.post_like }}</p>
 
       <div>
         <input type="text" id="commentContent" name="commentContent" v-model="comment_content" placeholder="comment" />
